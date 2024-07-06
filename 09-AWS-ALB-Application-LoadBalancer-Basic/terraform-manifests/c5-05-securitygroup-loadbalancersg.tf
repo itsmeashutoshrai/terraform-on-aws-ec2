94c6f5ead30141c4c5b9a1c558b4c85689ce8953
Ashutosh Rai <mahadevashu182@gmail.com>
2024-07-06 22:04:08 +0530

    modified:   09-AWS-ALB-Application-LoadBalancer-Basic/terraform-manifests/c5-05-securitygroup-loadbalancersg.tf

--- 09-AWS-ALB-Application-LoadBalancer-Basic/terraform-manifests/c5-05-securitygroup-loadbalancersg.tf
+++ 09-AWS-ALB-Application-LoadBalancer-Basic/terraform-manifests/c5-05-securitygroup-loadbalancersg.tf
@@ -25,5 +25,20 @@ module "loadbalancer_sg" {
     },
   ]
 }
+#In the context of the Terraform configuration you provided, from_port and to_port are parameters used to define the range of port numbers for which a security rule applies.
+#Here's a breakdown of what these terms mean:
 
+# from_port: This is the starting port number for the range of ports that you want to allow or deny traffic through. If you are specifying a single port, from_port will be equal to to_port.
+
+# to_port: This is the ending port number for the range of ports that you want to allow or deny traffic through. If you are specifying a single port, to_port will be equal to from_port.
+# from_port = 81: This indicates the starting port number is 81.
+# to_port = 81: This indicates the ending port number is 81.
+# protocol = 6: This represents the protocol being used. The value 6 corresponds to TCP (Transmission Control Protocol).
+# description = "Allow Port 81 from internet": This is a human-readable description of what the rule is doing.
+# cidr_blocks = "0.0.0.0/0": This allows traffic from any IP address (IPv4) on the internet.
+# In this case, the rule is allowing TCP traffic on port 81 from any IP address on the internet.
+
+# Security Group Rules
+# Ingress rules define the inbound traffic allowed to the resources associated with the security group.
+# Egress rules define the outbound traffic allowed from the resources associated with the security group.
 
